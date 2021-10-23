7175e9f201f4ccac6b43fa92c726be597824b94b
kai690 <k.green0519.ap@icloud.com>
2021-10-23 15:04:54 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '70c6556fdec63e6c1631f967c981921a1336dd4592b7ee68f6e8dce81356488f678876cafd7e62055283f3613dc5f6a49e280e4815d9b48f31ad89261d87c3e9'
